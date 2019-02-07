dd1d141af975b0984f141050197d77f1715d8429
crisuuu95 <47084752+crisuuu95@users.noreply.github.com>
2019-02-07 21:30:39 +0100

    finally

--- aplikacja.php
+++ aplikacja.php
@@ -43,19 +43,19 @@
 			<div id="wyloguj"><a style="text-align: center; color:white;" href="logout.php">Wyloguj się!</a></div>
 			<div class="forms-place">
 			<form action="query.php" method="post" id="main-form">
-						<span class="span-form"><label>id: </label><input class="align" type="text" name="id"></span>
-						<span class="span-form"><label>Imię: </label><input class="align" type="text" name="imie"></span>
+						<span class="span-form"><label>id:</label><input class="align" type="text" name="id"></span>
+						<span class="span-form"><label>Imię:</label><input class="align" type="text" name="imie"></span>
 						<span class="span-form"><label>Nazwisko: </label><input class="align" type="text" name="nazwisko"></span>
 						<span class="span-form"><label>Płeć: </label><input class="align" type="text" name="plec"></span>
-						<span class="span-form"><label>Wiek: </label><input class="align" type="text" name="wiek"></span>
-						<span class="span-form"><label>Waga: </label><input class="align" type="text" name="waga"></span>
-						<span class="span-form"><label>Wzrost </label><input class="align" type="text" name="wzrost"></span>
+						<span class="span-form"><label style="float:left;">Wiek: </label><input style="float:left;" class="align" type="text" name="wiek"> <label style="float:left;">   Od: </label><input style="float:left;" class="align" type="text" name="wiek_od"><label style="float:left;">   Do: </label><input style="float:left;" class="align" type="text" name="wiek_do"></span>
+						<span style="clear: both;"class="span-form"><label>Waga:</label><input class="align" type="text" name="waga"><label>   Od: </label><input class="align" type="text" name="waga_od"><label>   Do: </label><input class="align" type="text" name="waga_do"></span>
+						<span style="clear: both;" class="span-form"><label>Wzrost </label><input class="align" type="text" name="wzrost"><label>   Od: </label><input class="align" type="text" name="wzrost_od"><label>   Do: </label><input class="align" type="text" name="wzrost_do"></span>
 						<span class="span-form"><label>Kolor oczu: </label><input class="align" type="text" name="kolor_oczu"></span>
 						<span class="span-form"><label>Kolor włosów: </label><input class="align" type="text" name="kolor_wlosow"></span>
 						<span class="span-form"><label>Miasto: </label><input class="align" type="text" name="miasto"></span>
 						<span class="span-form"><label>Kraj: </label><input class="align" type="text" name="kraj"></span>
-						<span class="span-form"><label>Zarobki: </label><input class="align" type="text" name="zarobki"></span>
-						<span class="span-form"><label>Marka auta: </label><input class="align" type="text" name="marka_auta"></span>
+						<span class="span-form"><label>Zarobki: </label><input class="align" type="text" name="zarobki"><label>   Od: </label><input class="align" type="text" name="zarobki_od"><label>   Do: </label><input class="align" type="text" name="zarobki_do"></span>
+						<span style="clear: both;"class="span-form"><label>Marka auta: </label><input class="align" type="text" name="marka_auta"></span>
 						<span class="span-form"><label>Typ auta: </label><input class="align" type="text" name="typ_auta"></span>
 						<span class="span-form"><label>Silnik: </label><input class="align" type="text" name="silnik"></span>
 					
@@ -144,63 +144,47 @@
 						if(isset($_SESSION['miasto']))$miasto = $_SESSION['miasto'];
 						if(isset($_SESSION['kraj']))$kraj = $_SESSION['kraj'];
 						if(isset($_SESSION['zarobki']))$zarobki = $_SESSION['zarobki'];
+						if(isset($_SESSION['silnik']))$silnik = $_SESSION['silnik'];
 						if(isset($_SESSION['marka_auta']))$marka_auta = $_SESSION['marka_auta'];
 						if(isset($_SESSION['typ_auta']))$typ_auta = $_SESSION['typ_auta'];
-						if(isset($_SESSION['silnik'])) $silnik = $_SESSION['silnik'];
+						if(isset($_SESSION['wiek_od'])) $wiek_od = $_SESSION['wiek_od'];
+						if(isset($_SESSION['wiek_do'])) $wiek_do = $_SESSION['wiek_do'];
+						if(isset($_SESSION['waga_od'])) $waga_od = $_SESSION['waga_od'];
+						if(isset($_SESSION['waga_do'])) $waga_do = $_SESSION['waga_do'];
+						if(isset($_SESSION['wzrost_od'])) $wzrost_od = $_SESSION['wzrost_od'];
+						if(isset($_SESSION['wzrost_do'])) $wzrost_do = $_SESSION['wzrost_do'];
+						if(isset($_SESSION['zarobki_od'])) $zarobki_od = $_SESSION['zarobki_od'];
+						if(isset($_SESSION['zarobki_do'])) $zarobki_do = $_SESSION['zarobki_do'];
 						
 						$wh = array();
-						if(isset($id) && $id!="") 
-						{
-						   $wh[] = 'id = '.'"'.$id.'"'; 
-						  }
-						if(isset($imie) &&$imie!="") 
-						{
-							$wh[] = 'Imie = '.'"'.$imie.'"'; 
-						   }
-						if(isset($nazwisko) &&$nazwisko!="")
-						{
-						   $wh[] = 'Nazwisko = '.'"'.$nazwisko.'"'; 
-						   }
-						   if(isset($plec) &&$plec!="")
-						{
-						   $wh[] = 'Plec = '.'"'.$plec.'"'; 
-						   }if(isset($wiek) &&$wiek!="")
-						{
-						   $wh[] = 'Wiek = '.'"'.$wiek.'"'; 
-						   }if(isset($waga) &&$waga!="")
-						{
-						   $wh[] = 'Waga = '.'"'.$waga.'"'; 
-						   }if(isset($wzrost) &&$wzrost!="")
-						{
-						   $wh[] = 'Wzrost = '.'"'.$wzrost.'"'; 
-						   }if(isset($kolor_oczu) &&$kolor_oczu!="")
-						{
-						   $wh[] = 'Kolor_oczu = '.'"'.$kolor_oczu.'"'; 
-						   }if(isset($kolor_wlosow) &&$kolor_wlosow!="")
-						{
-						   $wh[] = 'Kolor_wlosow = '.'"'.$kolor_wlosow.'"'; 
-						   }if(isset($miasto) &&$miasto!="")
-						{
-						   $wh[] = 'Miasto = '.'"'.$miasto.'"'; 
-						   }if(isset($kraj) &&$kraj!="")
-						{
-						   $wh[] = 'Kraj = '.'"'.$kraj.'"'; 
-						   }if(isset($zarobki) &&$zarobki!="")
-						{
-						   $wh[] = 'Zarobki = '.'"'.$zarobki.'"'; 
-						   }if(isset($marka_auta) &&$marka_auta!="")
-						{
-						   $wh[] = 'Marka_auta = '.'"'.$marka_auta.'"'; 
-						   }if(isset($typ_auta) &&$typ_auta!="")
-						{
-						   $wh[] = 'Typ_auta = '.'"'.$typ_auta.'"'; 
-						   }
-						   if(isset($silnik) &&$silnik!="")
-						{
-						   $wh[] = 'Silnik = '.'"'.$silnik.'"';
-						   }
+						if(isset($id) && $id!="") $wh[] = 'id = '.'"'.$id.'"';
+						if(isset($imie) &&$imie!="") $wh[] = 'Imie = '.'"'.$imie.'"'; 
+						if(isset($nazwisko) &&$nazwisko!="") $wh[] = 'Nazwisko = '.'"'.$nazwisko.'"';
+						if(isset($plec) &&$plec!="") $wh[] = 'Plec = '.'"'.$plec.'"'; 
+						if(isset($wiek) &&$wiek!="") $wh[] = 'Wiek = '.'"'.$wiek.'"'; 
+					    if(isset($waga) &&$waga!="") $wh[] = 'Waga = '.'"'.$waga.'"'; 
+						if(isset($wzrost) &&$wzrost!="") $wh[] = 'Wzrost = '.'"'.$wzrost.'"'; 
+						if(isset($kolor_oczu) &&$kolor_oczu!="") $wh[] = 'Kolor_oczu = '.'"'.$kolor_oczu.'"'; 
+						if(isset($kolor_wlosow) &&$kolor_wlosow!="") $wh[] = 'Kolor_wlosow = '.'"'.$kolor_wlosow.'"'; 
+						if(isset($miasto) &&$miasto!="") $wh[] = 'Miasto = '.'"'.$miasto.'"'; 
+						if(isset($kraj) &&$kraj!="") $wh[] = 'Kraj = '.'"'.$kraj.'"'; 
+						if(isset($zarobki) &&$zarobki!="") $wh[] = 'Zarobki = '.'"'.$zarobki.'"'; 
+						if(isset($marka_auta) &&$marka_auta!="") $wh[] = 'Marka_auta = '.'"'.$marka_auta.'"'; 
+						if(isset($typ_auta) &&$typ_auta!="") $wh[] = 'Typ_auta = '.'"'.$typ_auta.'"'; 
+						if(isset($silnik) &&$silnik!="") $wh[] = 'Silnik = '.'"'.$silnik.'"';
 					
-					  
+					//
+					    if(isset($wiek_od) && $wiek_od!="") $wh[] = 'Wiek >= '.'"'.$wiek_od.'"';
+					    if(isset($wiek_do) && $wiek_do!="") $wh[] = 'Wiek <= '.'"'.$wiek_do.'"';
+					    if(isset($waga_od) && $waga_od!="") $wh[] = 'Waga >= '.'"'.$waga_od.'"';
+					    if(isset($waga_do) && $waga_do!="") $wh[] = 'Waga <= '.'"'.$waga_do.'"';
+					    if(isset($wzrost_od) && $wzrost_od!="") $wh[] = 'Wzrost >= '.'"'.$wzrost_od.'"';
+					    if(isset($wzrost_do) && $wzrost_do!="") $wh[] = 'Wzrost <= '.'"'.$wzrost_do.'"';
+					    if(isset($zarobki_od) && $zarobki_od!="") $wh[] = 'Zarobki >= '.'"'.$zarobki_od.'"';
+					    if(isset($zarobki_do) && $zarobki_do!="") $wh[] = 'Zarobki <= '.'"'.$zarobki_do.'"';
+						
+					
+						
 					
 					 
 						if (!empty($wh)) 
--- query.php
+++ query.php
@@ -17,6 +17,14 @@
 	$_SESSION['marka_auta'] = $_POST['marka_auta'];
 	$_SESSION['typ_auta'] = $_POST['typ_auta'];
 	$_SESSION['silnik'] = $_POST['silnik'];
+	$_SESSION['wiek_od']=$_POST['wiek_od'];
+	$_SESSION['wiek_do']=$_POST['wiek_do'];
+	$_SESSION['waga_od']=$_POST['waga_od'];
+	$_SESSION['waga_do']=$_POST['waga_do'];
+	$_SESSION['wzrost_od']=$_POST['wzrost_od'];
+	$_SESSION['wzrost_do']=$_POST['wzrost_do'];
+	$_SESSION['zarobki_od']=$_POST['zarobki_od'];
+	$_SESSION['zarobki_do']=$_POST['zarobki_do'];
 	
 	header('Location: aplikacja.php');
 	exit();
